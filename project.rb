770c7e365d2638bf0e6fc91b205ccc2c0f22269d
Alexis Walker <alexiswalker@Alexiss-MacBook-Pro.local>
2017-12-24 22:47:01 -0500

    started madlib project

---
+++ project.rb
@@ -0,0 +1,14 @@
+require 'yaml'
+class ProjectMain
+	def initialize(mad_lib_name)
+		@sentences = load_mad_lib(mad_lib_name)
+	end
+	def load_mad_lib(mad_lib_name)
+		begin 
+			YAML.load(File.read("#{mad_lib_name}.yml"))
+		rescue Errno::ENOENT => err
+			puts "Sorry, that mad lib no longer exists"
+		end
+	end
+end
+
